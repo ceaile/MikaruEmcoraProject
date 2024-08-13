842372e3662ad29f6c11fb13f91df08e13bb9f40
Ceaile <ceaile1994@protonmail.com>
2024-08-13 17:18:20 +0200

    comienzo de construccion de estilos en header, footer y home

--- MikaruEmcora.Frontend/src/app/app-routing.module.ts
+++ MikaruEmcora.Frontend/src/app/app-routing.module.ts
@@ -5,11 +5,14 @@ import { AboutMeComponent } from './pages/about-me/about-me.component';
 import { ContactMeComponent } from './pages/contact-me/contact-me.component';
 import { NotFoundComponent } from './pages/not-found/not-found.component';
 import { AdminComponent } from './pages/admin/admin.component';
+import { HomeComponent } from './pages/home/home.component';
 
 const routes: Routes = [
   { path: 'aboutMe', component: AboutMeComponent },
   { path: 'contactMe', component: ContactMeComponent },
-  { path: 'login', component: AdminComponent}, //componente login eliminado
+  { path: 'admin', component: AdminComponent },
+  { path: 'login', redirectTo: '/admin', pathMatch: 'full' }, //no entiendo muy bien lo del pathmatch full
+  { path: 'home', component: HomeComponent },
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: '**', component: NotFoundComponent }
 ];
